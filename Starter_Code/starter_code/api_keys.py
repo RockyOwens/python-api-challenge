0bd3a49e693edff6f8b9321f03e2bb25b8b35d5e
Rocky Owens <119636073+RockyOwens@users.noreply.github.com>
2023-01-31 13:33:16 -0600

    Update api_keys.py

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "75da7001c6d4c06251614b1d242583f3"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "e70fb1dd01b4199a226317731bd559d"
